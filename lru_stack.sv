80f2d7bb0fe0b8d20e4c34af892172cf4265bd8a
Pradeep Naraharirao <pradeepnaraharirao@Pradeeps-MacBook-Pro.local>
2020-06-20 23:07:45 -0700

    Init commit of LRU stack

--- lru_stack.sv
+++ lru_stack.sv
@@ -1,35 +1,58 @@
 //LRU
+module lru_stack
+#
+(
+parameter DEPTH      = 8,
+)
+(
+input  logic             updata_lru_stack,
+input  logic             clk,
+input  logic             rst_b,
+input  logic [DEPTH-1:0] accessed_blk,
+output logic [DEPTH-1:0] victim_blk,
+);
 
-always @(posedge clk)
- begin
-	if(update_sloc[0]==1'b1)
-		sloc[0]<=accessed_block;
-	for(j=1;j<(DEPTH-1);j=j+1)
-		begin
-		if(update_sloc[j])
-			sloc[j]<=sloc[j-1];
-		end
- end
-  
-always@(*)
-  begin
-	for(i=0;i<=(DEPTH-1);i=i+1)
-		begin
-			if(sloc[i]==accessed_block)
-				sloc_eq[i]=1'b1;
-			else
-				sloc_eq[i]=1'b0;
-		end		
-   end
-  
-always @(*)
+//Stored in decoded form
+logic [DEPTH-1:0] [DEPTH-1 : 0] sloc; 
+logic [DEPTH-1:0]               sloc_eq,update_sloc;
+//
+generate
+   for(i=0 ; i<DEPTH; i++)
+       if(i==0) begin
+            always @(posedge clk or negedge rst_b)
+                  if(~rst_b)
+                      sloc[i] <= '0;
+                  else if( update_lru_stack )
+                      sloc[i] <= accessed_blk;
+       end
+       else begin
+            always @(posedge clk or negedge rst_b)
+                  if(~rst_b)
+                      sloc[i] <= '0;
+                  else if( update_sloc[i] )
+                      sloc[i] <= sloc[i-1];
+       end
+endgenerate
+
+//Comparator as deep as DEPTH of LRU
+always_comb
 begin
-	if(update_lru_stack)
-		update_sloc[0]=1'b1;
-	else
-		update_sloc[0]=0;
-	for(p=1;i<=(DEPTH-1);p=i+1)
-		update_sloc[p]= (~ sloc_eq[p-1]) & update_sloc[p-1];
+for(i=0;i<=(DEPTH-1);i=i+1)
+   begin
+     if(sloc[i]==accessed_blk)
+        sloc_eq[i]=1'b1;
+     else
+        sloc_eq[i]=1'b0;
+   end		
+end
+  
+assign update_sloc[0]=update_lru_stack;;
+
+always_comb begin
+     for(int p=1;i<=(DEPTH-1);p=i+1)
+           update_sloc[p]= (~ sloc_eq[p-1]) & update_sloc[p-1];
 end
 
 assign victim =sloc[DEPTH-1];
+
+endmodule
